88f667775bfe238802e1b73c96ca0f0c28d78104
achen930 <annabellechen101@gmail.com>
2024-01-30 11:40:36 -0800

    Add NumberGroup

---
+++ NumberGroup.ts
@@ -0,0 +1,22 @@
+import { ISortable } from "./ISortable";
+
+export class NumberGroup implements ISortable {
+    data: number[];
+    get length(): number {
+        return this.data.length;
+    };
+    swap(leftPos: number, rightPos: number):void {
+        let leftNumber = this.data[leftPos];
+        let rightNumber = this.data[rightPos];
+
+        let temp = leftNumber;
+        leftNumber = rightNumber;
+        rightNumber = temp;
+    };
+    compare(leftPos: number, rightPos: number): boolean {
+        let leftNumber = this.data[leftPos];
+        let rightNumber = this.data[rightPos];
+
+        return leftNumber > rightNumber;
+    };
+}
